c045db8e08ca29c13657d04687fb35107c11d85d
DilankaSandeep <dilankacm@gmail.com>
2024-01-10 11:14:54 +0530

    add Resume section

---
+++ src/components/resume/ExtraCurricular.tsx
@@ -0,0 +1,7 @@
+export const ExtraCurricular = () => {
+    return (
+        <>
+        Extraa Curricular
+        </>
+    );
+};
--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -1,7 +1,89 @@
+import  {useState} from "react";
+import Title from "../titles/Title.tsx";
+import {Education} from "./Education.tsx";
+import {Skills} from "./Skills.tsx";
+import {ExtraCurricular} from "./ExtraCurricular.tsx";
+import {Experience} from "./Experience.tsx";
+
 export const Resume = () => {
+    const [educationData, setEducationData] = useState(true);
+    const [skillData, setSkillData] = useState(false);
+    const [experienceData, setExperienceData] = useState(false);
+    const [extraCurricularData, setExtraCurricularData] = useState(false);
+
     return (
-        <div id="resume">
-            Resume
-        </div>
+        <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
+            <div className="flex justify-center items-center text-center">
+                <Title title="4+ Years of Professional Experience" des="My Resume" />
+            </div>
+            <div>
+                <ul className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+                    <li
+                        onClick={() => {
+                            setEducationData(true);
+                            setSkillData(false);
+                            setExperienceData(false);
+                            setExtraCurricularData(false);
+                        }
+                        }
+                        className={`${
+                            educationData
+                                ? "border-nameTextColor rounded-lg"
+                                : "border-transparent"
+                        } resumeLi`}
+                    >
+                        Education
+                    </li>
+                    <li
+                        onClick={() =>{
+                            setEducationData(false);
+                            setSkillData(true) ;
+                            setExperienceData(false);
+                            setExtraCurricularData(false);
+                        }}
+                        className={`${
+                            skillData ? "border-nameTextColor" : "border-transparent"
+                        } resumeLi`}
+                    >
+                        Professional Skills
+                    </li>
+                    <li
+                        onClick={() =>{
+                            setEducationData(false);
+                            setSkillData(false);
+                            setExperienceData(true);
+                            setExtraCurricularData(false);
+                        }}
+                        className={`${
+                            experienceData
+                                ? "border-nameTextColor rounded-lg"
+                                : "border-transparent"
+                        } resumeLi`}
+                    >
+                        Experience
+                    </li>
+                    <li
+                        onClick={() =>{
+                            setEducationData(false);
+                            setSkillData(false) ;
+                            setExperienceData(false);
+                            setExtraCurricularData(true);
+                        }}
+                        className={`${
+                            extraCurricularData
+                                ? "border-nameTextColor rounded-lg"
+                                : "border-transparent"
+                        } resumeLi`}
+                    >
+                        Extra Curricular
+                    </li>
+                </ul>
+            </div>
+            {educationData && <Education />}
+            {skillData && <Skills />}
+            {experienceData && <ExtraCurricular/>}
+            {experienceData && <Experience />}
+
+        </section>
     );
 };
